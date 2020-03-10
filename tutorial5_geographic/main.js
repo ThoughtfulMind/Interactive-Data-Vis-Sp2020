cda6273eeaaa162e921986e4698d311a81ff9b0f
ThoughtfulMind <55453603+ThoughtfulMind@users.noreply.github.com>
2020-03-10 09:31:00 -0400

    update files

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -84,6 +84,42 @@ function init() {
     { latitude: 43.0731, longitude: -89.4012, },
     { latitude: 32.7157, longitude: -117.1611, },
     { latitude: 29.4241, longitude: -98.4936, },
+    { latitude: 40.745,longitude: -123.8695,}, 
+    { latitude: 38.4747,longitude:-121.3542,},
+    { latitude: 48.033,longitude:-121.8339,},
+    { latitude: 41.2545,longitude:-95.9758,}, 
+    { latitude: 43.9088,longitude:-71.826,},
+    { latitude:27.9904,longitude:-82.3018,},
+    { latitude: 39.0916,longitude:-120.8039,},
+    { latitude: 37.563,longitude:-122.3255,},
+    { latitude: 27.3364,longitude:-82.5307,},
+    { latitude: 38.578,longitude:-122.9888,},
+    { latitude: 45.775,longitude:-118.7606,},
+    { latitude: 33.8034,longitude:-84.3963,},
+    { latitude: 45.547,longitude:-123.1386,},
+    { latitude: 42.1767,longitude:-71.1449,}, 
+    { latitude: 37.8715,longitude:-122.273,}, 	
+    { latitude: 33.2918,longitude:-112.4291,}, 
+    { latitude: 35.8032,longitude:-78.5661}, 
+    { latitude: 41.122,longitude:-73.7949},
+    { latitude: 41.122,longitude:-73.7949},
+    { latitude: 33.7879,longitude:-117.85311},
+    { latitude: 37.8534,longitude:-121.9018},
+    { latitude: 40.9263, longitude:-74.077,}, 
+    { latitude: 29.7752,longitude:-95.3103},
+    { latitude: 37.7749,longitude:-122.4194},
+    { latitude: 36.0796,longitude:-115.094,},
+    { latitude: 29.5693,longitude:-95.8143,},
+    { latitude: 47.1981,longitude:-119.3732,},
+    { latitude: 30.769,	longitude:-86.9824,},
+    { latitude: 35.9179,longitude:-86.8622,},
+    { latitude:	40.7128,longitude:-74.006,},
+    { latitude: 47.7511,longitude:-120.7401,},
+    { latitude: 39.1547,longitude:-77.2405,},
+    { latitude: 42.3601,longitude:-71.0589,},
+    { latitude: 39.7392, longitude:-104.9903,},
+    { latitude: 39.5912,longitude:-106.064,},
+
       ];
   svg
     .selectAll("circle")
@@ -91,7 +127,7 @@ function init() {
     .join("circle")
     .attr("r", 20)
     .attr("fill", "red")
-    .attr("fill-opacity", 0.6)
+    .attr("fill-opacity", 0.4)
     .attr("stroke", "#B22222")
     .attr("stroke-width", 1)
     .attr("transform", d => {
